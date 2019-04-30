3c7dcb4a0c7021a6b9500b91f1e3a1dc9077fef8
muthurajag <muthuraja.g@newgendigital.com>
2019-04-30 09:41:47 +0530

    First Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserEvent } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private theInAppBrowser: InAppBrowser) {
 
   }
+
+  public openWithInAppBrowser(url : string){
+    
+      let target = "_blank";
+      const close_inapp = this.theInAppBrowser.create(url, target);
+   
+      close_inapp.on("exit").subscribe((ev: InAppBrowserEvent) => {
+        alert("fdsfs");
+      });
+  }
+
+  
 
 }
